7b3ceea8dbd88529e7c9df99d66083ec9c071f1c
Your Name <you@example.com>
2026-06-12 12:39:49 +0700

    sửa code task 3

--- src/main/java/com/rikkeisoft/bank/repository/TokenBlackListRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rikkeisoft.bank.repository;
-
-import com.rikkeisoft.bank.entity.TokenBlackList;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TokenBlackListRepository extends JpaRepository<TokenBlackList, Long> {
-    boolean existsByAccessToken(String accessToken);
-}
